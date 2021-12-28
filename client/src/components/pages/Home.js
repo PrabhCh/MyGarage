d72c875986ab5fb0169adc401552dc9bdb750b19
Prabh Chaudhary <prabh2012@hotmail.com>
2021-12-27 20:44:13 -0500

    Added Buy & Sell buttons on Home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='grid-2 text-center'>
+      <div>
+        <Link className='large box-buy' to='/buy'>
+          Buy Car
+        </Link>
+      </div>
+      <div>
+        <Link className='large text-danger box-sell' to='/sell'>
+          Sell Car
+        </Link>
+      </div>
     </div>
   );
 };
